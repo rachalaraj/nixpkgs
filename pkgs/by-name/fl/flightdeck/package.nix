11444db57959aba57b464b721d7c652e788d2f4c
Rachala Raj <rachaalaraj@gmail.com>
2026-09-03 16:11:08 +0530

    caelestia-shell-testing: aggregate all AstraSuite packages

---
+++ pkgs/by-name/fl/flightdeck/package.nix
@@ -0,0 +1,62 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  ninja,
+  pkg-config,
+  qt6,
+  nix-update-script,
+  testers,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "flightdeck";
+  version = "1.0.0";
+
+  __structuredAttrs = true;
+  strictDeps = true;
+
+  src = fetchFromGitHub {
+    owner = "AstraSuite";
+    repo = "FlightDeck";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-pZNpUpnKheQmNW5hPmx6a90eIUBje/FkdOC9Nb+9zSA=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+    pkg-config
+    qt6.wrapQtAppsHook
+    qt6.qtshadertools
+  ];
+
+  buildInputs = [
+    qt6.qtbase
+    qt6.qtdeclarative
+    qt6.qtshadertools
+    qt6.qtsvg
+  ];
+
+  cmakeFlags = [
+    (lib.cmakeFeature "FLIGHTDECK_VERSION" finalAttrs.version)
+  ];
+
+  passthru = {
+    updateScript = nix-update-script { };
+    tests.version = testers.testVersion {
+      package = finalAttrs.finalPackage;
+      command = "QT_QPA_PLATFORM=offscreen flightdeck --version";
+    };
+  };
+
+  meta = {
+    description = "Hyprland configuration manager for Caelestia Shell";
+    homepage = "https://github.com/AstraSuite/FlightDeck";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ rachalaraj ];
+    mainProgram = "flightdeck";
+    platforms = lib.platforms.linux;
+  };
+})
